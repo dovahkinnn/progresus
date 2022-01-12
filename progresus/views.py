ee03f658002b12c54d9b5289b96a4c5803b7708a
dovahkinnn <sherlockhakan72@gmail.com>
2022-01-12 22:31:51 +0300

    Update views.py

--- progresus/views.py
+++ progresus/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login,authenticate,logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from lcu_driver import Connector
 
 
 
@@ -143,35 +144,99 @@ def logout_page(request):
 
 @csrf_exempt
 def ContentLol(request):
+    connector = Connector()
 
-    if request.user.is_authenticated != True :
-        return redirect('Home')
+    # Creates 5v5 Practice Tool
+    async def createLobby(connection):
+        data = {
+            "customGameLobby": {
+                "configuration": {
+                    "gameMode": "PRACTICETOOL",
+                    "gameMutator": "",
+                    "gameServerRegion": "",
+                    "mapId": 11,
+                    "mutators": {
+                        "id": 1
+                    },
+                    "spectatorPolicy": "AllAllowed",
+                    "teamSize": 5,
+                },
+                "lobbyName": "League of Poro's Practice Tool",
+                "lobbyPassword": ""
+            },
+            "isCustom": True,
+        }
+        # make the request to switch the lobby
+        lobby = await connection.request('post', '/lol-lobby/v2/lobby', data=data)
+
+        # if HTTP status code is 200 the lobby was created successfully
+        if lobby.status == 200:
+            print('The lobby was created correctly')
+        else:
+            print('Whops, Yasuo died again.')
 
 
-    id=request.user.id
-    lol_nickname=request.POST.get("lol_nickname", "")
-    format= database.child("users").get()
-    for i in format:
-        
-        if i.val()["id"] == id:
-            a=i.key()
-            database.child("users").child(a).update({"lol_nickname": lol_nickname})
-    
-    format= database.child("users").get()
-    istek= database.child("istekler").get()
-    Database_All_Data_Value=format.val()
-    Database_istek_Value=istek.val()
-    context={
-        "Data_For_User":Database_All_Data_Value,
-        "Data_For_istek":Database_istek_Value
+    # Contacts LCU API to add bots
+    async def executeAddBot(connection, data):
+        res = await connection.request('post', '/lol-lobby/v1/lobby/custom/bots', data=data)
+        if res.status == 204:
+            print('Bot added')
+        else:
+            print('Whops, Yasuo died again.')
+
+    # Selects which bots to add and adds them to an existing lobby
+    async def addBots(connection):
+        ids = [1, 3, 8, 10, 11]
+
+        # add bots to the player's team
+        for id in ids[:4]:
+            data = {
+                "botDifficulty": "EASY",
+                "championId": id,
+                "teamId": "100"
+            }
+            await executeAddBot(connection, data)
+
+        # add bots to the opposite team
+        for id in ids:
+            data = {
+                "botDifficulty": "EASY",
+                "championId": id,
+                "teamId": "200"
+            }
+            await executeAddBot(connection, data)
 
 
-    }
+    # fired when LCU API is ready to be used
+    @connector.ready
+    async def connect(connection):
+        print('LCU API is ready to be used.')
+
+        # check if the user is already logged into his account
+        summoner = await connection.request('get', '/lol-summoner/v1/current-summoner')
+        if summoner.status != 200:
+            print('Please login into your account.')
+        else:
+            print('Switching the lobby type.')
+            await createLobby(connection)
+            await addBots(connection)
 
 
-    return render(request,"content.html",context)
+    # fired when League Client is closed (or disconnected from websocket)
+    @connector.close
+    async def disconnect(_):
+        print('The client have been closed!')
+
+    # starts the connector
+    connector.start()
+  
+  
+  
+
+    return redirect('Home')
 
 def User_Challenge(request):
+  
     istek_alan = request.GET.get('abc')
     istek_gönderen_id=request.user.id
     istek_gönderen=request.user.username
