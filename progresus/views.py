71192ac7d7cf7664fa76eba99aed42dd60249bce
dovahkinnn <sherlockhakan72@gmail.com>
2022-03-10 00:01:52 +0300

    Aa

--- progresus/views.py
+++ progresus/views.py
@@ -20,15 +20,7 @@ import json
 # Create your views here.
 
 
-config={
-  "apiKey": "AIzaSyAwH2uSz9iiUTBa8W4NMf-3Osu7spMmbz4",
-  "authDomain": "django-c0d88.firebaseapp.com",
-  "databaseURL": "https://django-c0d88-default-rtdb.firebaseio.com",
-  "projectId": "django-c0d88",
-  "storageBucket": "django-c0d88.appspot.com",
-  "messagingSenderId": "290546126178",
-  "appId": "1:290546126178:web:68e8cefcd33dbf6259b03b",
-  "measurementId": "G-F5G1043X39"
+config
   
 }
 
